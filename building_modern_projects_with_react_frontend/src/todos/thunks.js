b93b56fda00b2bdd44cb86174e75c3158217cd49
Jeffrin Mathew Benny <jeffrinmathewbenny@gmail.com>
2022-05-31 15:22:04 +0530

    Adds the post end point for todos

--- building_modern_projects_with_react_frontend/src/todos/thunks.js
+++ building_modern_projects_with_react_frontend/src/todos/thunks.js
@@ -2,6 +2,7 @@ import {
   loadTodosInProgress,
   loadTodosSuccess,
   loadTodosFailure,
+  createTodo,
 } from './actions';
 
 export const loadTodos = () => async (dispatch, getState) => {
@@ -12,6 +13,23 @@ export const loadTodos = () => async (dispatch, getState) => {
     dispatch(loadTodosSuccess(todos));
   } catch (e) {
     dispatch(loadTodosFailure());
+    dispatch(displayAlert(e));
+  }
+};
+
+export const addTodoRequest = text => async dispatch => {
+  try {
+    const body = JSON.stringify({ text });
+    const response = await fetch('http://localhost:8080/todos', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body,
+    });
+    const todo = await response.json();
+    dispatch(createTodo(todo));
+  } catch (e) {
     dispatch(displayAlert(e));
   }
 };
