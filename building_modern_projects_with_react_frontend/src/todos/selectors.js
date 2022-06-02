a8da0fc0d9eb00f93644f04d37a3f00426e401b3
Jeffrin Mathew Benny <jeffrinmathewbenny@gmail.com>
2022-06-02 09:24:47 +0530

    Add reselect package and reselctors

--- building_modern_projects_with_react_frontend/src/todos/selectors.js
+++ building_modern_projects_with_react_frontend/src/todos/selectors.js
@@ -1,3 +1,5 @@
+import { createSelector } from 'reselect';
+
 export const getTodos = state => {
   return state.todos.data;
 };
@@ -5,3 +7,11 @@ export const getTodos = state => {
 export const getTodosLoading = state => {
   return state.todos.isLoading;
 };
+
+export const getIncompleteTodos = createSelector(getTodos, todos =>
+  todos.filter(todo => !todo.isCompleted)
+);
+
+export const getCompletedTodos = createSelector(getTodos, todos =>
+  todos.filter(todo => todo.isCompleted)
+);
