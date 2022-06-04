4deee6c174caeb0a10320fa27a527fc9b0a34804
Jeffrin Mathew Benny <jeffrinmathewbenny@gmail.com>
2022-06-04 18:25:48 +0530

    Add conditional styles by extending the sytle components

--- building_modern_projects_with_react_frontend/src/todos/TodoListItem.js
+++ building_modern_projects_with_react_frontend/src/todos/TodoListItem.js
@@ -4,14 +4,17 @@ import styled from 'styled-components';
 const TodoItemContainer = styled.div`
   background: #fff;
   border-radius: 8px;
-  border-bottom: ${props =>
-    new Date(props.createdAt) > new Date(Date.now() - 8640000 * 5)
-      ? 'none'
-      : '2px solid red'};
   margin-top: 8px;
   padding: 16px;
   position: relative;
   box-shadow: 0 4px 8px grey;
+`;
+
+const TodoItemContainerWithWarning = styled(TodoItemContainer)`
+  border-bottom: ${props =>
+    new Date(props.createdAt) > new Date(Date.now() - 8640000 * 5)
+      ? 'none'
+      : '2px solid red'};
 `;
 
 const ButtonsContainer = styled.div`
@@ -20,7 +23,7 @@ const ButtonsContainer = styled.div`
   bottom: 12px;
 `;
 
-const CompletedButton = styled.button`
+const Button = styled.button`
   font-size: 16px;
   padding: 8px;
   border: none;
@@ -28,24 +31,23 @@ const CompletedButton = styled.button`
   outline: none;
   cursor: pointer;
   display: inline-block;
+`;
+
+const CompletedButton = styled(Button)`
   background-color: #22ee22;
 `;
 
-const RemoveButton = styled.button`
-  font-size: 16px;
-  padding: 8px;
-  border: none;
-  border-radius: 8px;
-  outline: none;
-  cursor: pointer;
-  display: inline-block;
+const RemoveButton = styled(Button)`
   background-color: #ee2222;
   margin-left: 8px;
 `;
 
 const TodoListItem = ({ todo, onRemovePressed, onCompletedPressed }) => {
+  const Container = todo.isCompleted
+    ? TodoItemContainer
+    : TodoItemContainerWithWarning;
   return (
-    <TodoItemContainer createdAt={todo.createdAt}>
+    <Container createdAt={todo.createdAt}>
       <h3>{todo.text}</h3>
       <p>Created At:&nbsp;{new Date(todo.createdAt).toLocaleDateString()}</p>
       <ButtonsContainer>
@@ -58,7 +60,7 @@ const TodoListItem = ({ todo, onRemovePressed, onCompletedPressed }) => {
           Remove
         </RemoveButton>
       </ButtonsContainer>
-    </TodoItemContainer>
+    </Container>
   );
 };
 
